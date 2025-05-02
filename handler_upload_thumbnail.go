31695074c019a6cfc0e85c3421fa4d5b02783620
Mauro <mbassini@outlook.com>
2025-05-02 16:18:28 -0300

    Invalidate cache by changing file name with random bytes

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -49,7 +49,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	assetPath := getAssetPath(videoID, mediaType)
+	assetPath := getAssetPath(mediaType)
 	assetDiskPath := cfg.getAssetDiskPath(assetPath)
 
 	destinationFile, err := os.Create(assetDiskPath)
