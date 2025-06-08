0f3021430d05ce8c979392de1a689b0729e2ac2b
Mauro <mbassini@outlook.com>
2025-06-08 20:58:13 -0300

    PresignedURL

--- assets.go
+++ assets.go
@@ -40,8 +40,8 @@ func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
 }
 
-func (cfg apiConfig) getObjectURL(aspectRatio, assetPath string) string {
-	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s/%s", cfg.s3Bucket, cfg.s3Region, aspectRatio, assetPath)
+func (cfg apiConfig) getObjectURL(key string) string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
 }
 
 func mediaTypeToExtension(mediaType string) string {
